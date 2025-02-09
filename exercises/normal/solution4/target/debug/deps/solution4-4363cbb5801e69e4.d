f8366cc84526a4d4640353e4e42f882caa740b28
T4t4KAU <huangwenxuan271828@163.com>
2025-02-09 12:12:03 +0800

    update

--- exercises/normal/solution4/target/debug/deps/solution4-4363cbb5801e69e4.d
+++
@@ -1,6 +0,0 @@
-/home/hwx/codes/Rust-Professional/exercises/normal/solution4/target/debug/deps/solution4-4363cbb5801e69e4: src/main.rs src/rec_mc.rs
-
-/home/hwx/codes/Rust-Professional/exercises/normal/solution4/target/debug/deps/solution4-4363cbb5801e69e4.d: src/main.rs src/rec_mc.rs
-
-src/main.rs:
-src/rec_mc.rs:
